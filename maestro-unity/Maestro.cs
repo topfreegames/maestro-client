8fb1ba3769279e58fdef329ebfca869d544e9d52
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-08-11 21:33:56 -0300

    update maestro.cs

--- maestro-unity/Maestro.cs
+++ maestro-unity/Maestro.cs
@@ -1,101 +1,155 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 using System.Collections;
 using System.Runtime.InteropServices;
 using System;
+using TFG.Core;
 
-public class MaestroClient : MonoBehaviour
-{
-	[UnmanagedFunctionPointer(CallingConvention.StdCall)]
-	private delegate void DebugLog(string log);
-	private static readonly DebugLog debugLog = DebugWrapper;
-	private static readonly IntPtr functionPointer = Marshal.GetFunctionPointerForDelegate(debugLog);
-	private static IntPtr maestroClient = IntPtr.Zero;
+namespace TFG.Modules {
+    public class Maestro : AutoCreateManager<Maestro> {
+        public static bool AutoPingEnable = true;
 
-	private static void DebugWrapper(string log)
-	{
-		Debug.Log(log);
-	}
+        public enum Status {
+            Ready,
+            Occupied,
+            Terminating,
+            Terminated,
+        }
 
-	private static void Init(string maestroApiUrl, int pingInterval){
-		Debug.Log ("Maestro client initializing!");
-		LinkDebugInternal (functionPointer);
-		maestroClient = ClientInternal (maestroApiUrl, pingInterval);
-	}
+        [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+        private delegate void DebugLog(string log);
+		[UnmanagedFunctionPointer(CallingConvention.StdCall)]
+		public delegate void OnTerminating();
 
-	public static void Initialize(string maestroApiUrl){
-		Init (maestroApiUrl, 30);
-		if (InitializeInternal(maestroClient)){
-			Debug.Log("Maestro client initialized successfully");
-		} else {
-			Debug.LogError ("Maestro client failed to initialize =/");
-		}
-	}
+        private static readonly DebugLog debugLog = DebugWrapper;
+        private static readonly IntPtr functionPointer = Marshal.GetFunctionPointerForDelegate(debugLog);
+        private static IntPtr maestroClient = IntPtr.Zero;
+        private static float PingInterval = 30;
+        private static bool initialized = false;
 
-	public static void Initialize(string maestroApiUrl, string scheduler, string id){
-		Init (maestroApiUrl, 30);
-		if (InitializeInternal(maestroClient,scheduler,id)){
-			Debug.Log("Maestro client initialized successfully");
-		} else {
-			Debug.LogError ("Maestro client failed to initialize =/");
-		}
-	}
+        private static void DebugWrapper(string log) {
+#if DEBUG
+            Debug.Log("Maestro:" + log);
+#endif
+        }
 
-	public static string GetAddress(){
-		IntPtr ptr = GetAddressInternal (maestroClient);
-		return Marshal.PtrToStringAuto (ptr);
-	}
+        public static string Address {
+            get {
+                if (!IsInitialized) return null;
 
-	public static int GetPingInterval() {
-		return GetPingIntervalInternal (maestroClient);
-	}
+                IntPtr ptr = GetAddressInternal(maestroClient);
+                return Marshal.PtrToStringAuto(ptr);
+            }
+        }
 
-	public static void SetPingInterval(int pingInterval) {
-		SetPingIntervalInternal (maestroClient, pingInterval);
-	}
+        private static bool IsInitialized {
+            get {
+                if (!initialized) {
+                    Debug.LogError("Maestro: calling methods before initialization!");
+                }
+                return initialized;
+            }
+        }
 
-	public static bool Ping(){
-		return PingInternal (maestroClient);
-	}
+		public static void Initialize(string apiUrl, string scheduler = null, string id = null, bool autoPing = true, float pingInterval = 30.0f, OnTerminating onTerminatingFunction = null) {
+			if (!initialized) {
+				LinkDebugInternal (functionPointer);
+				if (onTerminatingFunction != null) {
+					OnTerminating terminatingFunction = onTerminatingFunction;
+					IntPtr terminateFunctionPointer = Marshal.GetFunctionPointerForDelegate (terminatingFunction);
+					LinkOnTerminatingInternal (terminateFunctionPointer);
+				}
+				maestroClient = ClientInternal (apiUrl, 0);
 
-	public static bool RoomOccupied() {
-		return RoomOccupiedInternal (maestroClient);
-	}
+				var initilized = false;
 
-	public static bool RoomReady() {
-		return RoomReadyInternal (maestroClient);
-	}
+				if (string.IsNullOrEmpty (scheduler) || string.IsNullOrEmpty (id)) {
+					initilized = InitializeInternal (maestroClient);
+				} else {
+					initilized = InitializeInternal (maestroClient, scheduler, id);
+				}
 
-	public static bool RoomTerminated() {
-		return RoomTerminatedInternal (maestroClient);
-	}
+				if (initilized) {
+					Debug.Log ("Maestro client initialized successfully");
+				} else {
+					Debug.LogError ("Maestro client failed to initialize 😿 : " + apiUrl);
+				}
 
-	public static bool RoomTerminating() {
-		return RoomTerminatingInternal (maestroClient);
-	}
+				AutoPingEnable = autoPing;
+				PingInterval = pingInterval;
+				Instance.StartCoroutine (Instance.PingCoroutine ());
 
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_get_address")]
-	private static extern IntPtr GetAddressInternal(IntPtr obj);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_get_ping_interval")]
-	private static extern int GetPingIntervalInternal(IntPtr obj);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_initialize")]
-	private static extern bool InitializeInternal(IntPtr obj);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_initialize_with_scheduler_and_id")]
-	private static extern bool InitializeInternal(IntPtr obj, string scheduler, string id);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="set_debug")]
-	private static extern void LinkDebugInternal([MarshalAs(UnmanagedType.FunctionPtr)]IntPtr debugCal);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_create_client_with_ping_interval")]
-	private static extern IntPtr ClientInternal (string maestroApiUrl, int pingInterval);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_ping")]
-	private static extern bool PingInternal (IntPtr obj);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_room_occupied")]
-	private static extern bool RoomOccupiedInternal (IntPtr obj);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_room_ready")]
-	private static extern bool RoomReadyInternal (IntPtr obj);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_room_terminated")]
-	private static extern bool RoomTerminatedInternal (IntPtr obj);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_room_terminating")]
-	private static extern bool RoomTerminatingInternal (IntPtr obj);
-	[DllImport ("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint="internal_set_ping_interval")]
-	private static extern void SetPingIntervalInternal(IntPtr obj, int pingInterval);
+				initialized = true;
+			}
+        }
+
+        public static void Ping() {
+            if (!IsInitialized) return;
+            PingInternal(maestroClient);
+        }
+
+        public static void UpdateStatus(Status status) {
+            if (!IsInitialized) return;
+
+            switch (status) {
+                case Status.Ready:
+                    RoomReadyInternal(maestroClient);
+                    break;
+                case Status.Occupied:
+                    RoomOccupiedInternal(maestroClient);
+                    break;
+                case Status.Terminating:
+                    RoomTerminatingInternal(maestroClient);
+                    break;
+                case Status.Terminated:
+                    RoomTerminatedInternal(maestroClient);
+                    break;
+            }
+        }
+
+        private IEnumerator PingCoroutine() {
+            while (AutoPingEnable) {
+                yield return new WaitForSeconds(PingInterval);
+				Ping();
+            }
+        }
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_get_address")]
+        private static extern IntPtr GetAddressInternal(IntPtr obj);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_get_ping_interval")]
+        private static extern int GetPingIntervalInternal(IntPtr obj);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_initialize")]
+        private static extern bool InitializeInternal(IntPtr obj);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_initialize_with_scheduler_and_id")]
+        private static extern bool InitializeInternal(IntPtr obj, string scheduler, string id);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "set_debug")]
+        private static extern void LinkDebugInternal([MarshalAs(UnmanagedType.FunctionPtr)] IntPtr debugCal);
+
+		[DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "set_on_terminating")]
+		private static extern void LinkOnTerminatingInternal([MarshalAs(UnmanagedType.FunctionPtr)] IntPtr onTerminatingCal);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_create_client_with_ping_interval")]
+        private static extern IntPtr ClientInternal(string maestroApiUrl, int pingInterval);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_ping")]
+        private static extern bool PingInternal(IntPtr obj);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_room_occupied")]
+        private static extern bool RoomOccupiedInternal(IntPtr obj);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_room_ready")]
+        private static extern bool RoomReadyInternal(IntPtr obj);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_room_terminated")]
+        private static extern bool RoomTerminatedInternal(IntPtr obj);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_room_terminating")]
+        private static extern bool RoomTerminatingInternal(IntPtr obj);
+
+        [DllImport("libmaestro", CallingConvention = CallingConvention.Cdecl, EntryPoint = "internal_set_ping_interval")]
+        private static extern void SetPingIntervalInternal(IntPtr obj, int pingInterval);
+    }
 }
