06e4594f5ebb2d78b71e6f8fb7048f79412c1a72
Henrique Rodrigues <henrique.93.rodrigues@gmail.com>
2017-09-20 09:46:54 -0300

    install script for alpine

--- maestro-node/install.sh
+++ maestro-node/install.sh
@@ -2,10 +2,10 @@ RESTCLIENT_VERSION=0.4.4
 boost_version=1.63.0
 boost_dir=boost_1_63_0
 
-if [ $(uname) == "Darwin" ]; then
+if [ (command -v brew) ]; then
   brew tap mrtazz/oss
   brew install restclient-cpp boost
-else
+elif [ (command -v apt-get) ]; then
   dir=$(pwd)
   apt-get update
   apt-get -y install build-essential cmake libboost-dev curl autoconf autogen \ 
@@ -17,7 +17,18 @@ else
   make install 
   rm -rf /tmp /var/lib/apt
   cd $dir
+elif [ (command -v apk) ]; then
+  dir=$(pwd)
+  apk update
+  apk add alpine-sdk cmake boost curl autoconf automake \ 
+    libtool libcurl curl-dev
+  cd /tmp 
+  curl -L -o restclient.tar.gz https://github.com/mrtazz/restclient-cpp/archive/$RESTCLIENT_VERSION.tar.gz 
+  tar -zxf restclient.tar.gz && cd restclient-cpp-$RESTCLIENT_VERSION && ./autogen.sh
+  ./configure 
+  make install 
+  cd $dir
+  rm -rf /tmp
 fi
 
 npm install -g node-gyp
-node-gyp rebuild
