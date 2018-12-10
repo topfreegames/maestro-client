137d895150cc2b70b721409d37b6a42aa969132b
Leonardo Hahn <leonardo.hahn@tfgco.com>
2018-12-10 16:47:47 -0200

    Build libraries to only depend dynamically in libc++. In Linux link against libc++ as well instead of libstdc++ (in order to fix a bug in unity 2018 beta)

--- build_libs.sh
+++ build_libs.sh
@@ -1,11 +1,13 @@
 OS=`uname | awk '{ print tolower($0) }'`
 if [[ $OS == 'darwin' ]]; then
     echo "Building .bundle lib..."
-    rm -f CMakeCache.txt
-    cmake .
-    make
+    rm -rf _builds/mac
+    mkdir -p _builds/mac
+    conan install . -if _builds/mac
+    cmake -DCMAKE_BUILD_TYPE=Release -H. -B_builds/mac -DBUILD_SHARED_LIBS=ON -DCMAKE_PREFIX_PATH=../deps/restclient-cpp/mac
+    cmake --build _builds/mac
     echo "Copying built .bundle lib..."
-    cp lib/libmaestro.dylib maestro-unity/Plugins/libmaestro.bundle
+    cp _builds/mac/lib/libmaestro.dylib maestro-unity/Plugins/libmaestro.bundle
     echo "Done"
 else
     echo "Not running in OSX, skipping .bundle lib build"
@@ -13,9 +15,9 @@ fi
 
 echo "Building .so lib..."
 echo "Building image..."
-docker build -f Dockerfile-builder -t maestro-example-builder .
+docker build -f Dockerfile-builder -t maestro-example-builder:latest .
 echo "Running commands..."
-docker run -v $(pwd):/app-src maestro-example-builder:latest -c "rm -f CMakeCache.txt && cmake . && make"
+docker run -v $(pwd):/app-src maestro-example-builder:latest -c "rm -rf _builds/linux && mkdir -p _builds/linux && conan install . -if _builds/linux && cmake -H. -B_builds/linux -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=../deps/restclient-cpp/linux && cmake --build _builds/linux"
 echo "Copying built .so lib..."
-cp lib/libmaestro.so maestro-unity/Plugins/libmaestro.so
+cp _builds/linux/lib/libmaestro.so maestro-unity/Plugins/libmaestro.so
 echo "Done"
